552f4b9b5a8a793b47f6c50a98bba3ea56941968
hdaley <howarddaley@gmail.com>
2017-07-03 11:07:47 +0100

    Update weightedtree.js

--- src/theme/weightedtree.js
+++ src/theme/weightedtree.js
@@ -10,7 +10,7 @@ vizuly.theme.weighted_tree = function (viz) {
         "Axiis" : {
             name: "Axiis",                          // Skin Name
             label_color: "#333",                    // Color of the center label
-            link_colors: ["#bd0026", "#fecc5c", "#fd8d3c", "#f03b20", "#B02D5D",
+            link_colors: ["#d7667d", "#fecc5c", "#fd8d3c", "#f03b20", "#B02D5D",
                 "#9B2C67", "#982B9A", "#692DA7", "#5725AA", "#4823AF",
                 "#d7b5d8", "#dd1c77", "#5A0C7A", "#5A0C7A"],
             link_stroke: function (d, i) {
