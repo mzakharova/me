2f62130b84e9c920eb30a2b645581dfaa423ac69
mzakharova <zakharovam@parliament.uk>
2017-07-03 11:13:09 +0100

    Update weightedtree.js

--- src/viz/weightedtree.js
+++ src/viz/weightedtree.js
@@ -60,7 +60,7 @@ vizuly.viz.weighted_tree = function (parent) {
     viz.on("data_change.internal",onDataChanged);
 
 
-    var colors = ["#bd0026", "#fecc5c", "#fd8d3c", "#f03b20", "#B02D5D",
+    var colors = ["#d7667d", "#fecc5c", "#fd8d3c", "#f03b20", "#B02D5D",
         "#9B2C67", "#982B9A", "#692DA7", "#5725AA", "#4823AF",
         "#d7b5d8", "#dd1c77", "#5A0C7A", "#5A0C7A"];
 
